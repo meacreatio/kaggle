34f1bb5447391dd12fff4ff218e3c59d2f6bce9a
meacreatio <>
2018-01-09 14:00:00 -0800

    update print_na_count

--- kaggle/Utils/UtilsDataFrame.py
+++ kaggle/Utils/UtilsDataFrame.py
@@ -4,5 +4,9 @@ import pandas as pd
 def delete_column(df, name):
     df.drop([name], axis=1, inplace=True, errors='ignore')
 
+
 def print_na_count(df):
-    print(df.isnull().sum())
+    for column in df:
+        total_na = df[column].isnull().sum()
+        if total_na > 0:
+            print("%s -> %s" % (column, total_na))
