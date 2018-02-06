f23329f390bab7f617217311cbefac2e414a1d19
Matt Pavlovich <meacreatio@gmail.com>
2018-02-06 15:54:29 -0800

    data visualization

---
+++ MLM/General/5.7.py
@@ -0,0 +1,13 @@
+from pandas import read_csv
+from matplotlib import pyplot
+from pandas.plotting import scatter_matrix
+
+filename = "pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+skew = data.skew()
+print(skew)
+data.plot(kind='density', subplots=True, layout=(3,3), sharex=False)
+# scatter_matrix(data)
+pyplot.show()
+
