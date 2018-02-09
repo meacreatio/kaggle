df0cd654129c3423ad207989c4e8490cf5f1db75
Matt Pavlovich <meacreatio@gmail.com>
2018-02-08 19:09:54 -0800

    boston housing complete

--- MLM/General/BostonHousing.py
+++ MLM/General/BostonHousing.py
@@ -173,4 +173,31 @@ fig.suptitle('Scaled Ensemble Algorithm Comparison')
 ax = fig.add_subplot(111)
 pyplot.boxplot(results)
 ax.set_xticklabels(names)
-pyplot.show()
+# pyplot.show()
+
+# Tune scaled GBM
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+param_grid = dict(n_estimators=numpy.array([50,100,150,200,250,300,350,400]))
+model = ExtraTreesRegressor(random_state=seed)
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# prepare the model
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+model = GradientBoostingRegressor(random_state=seed, n_estimators=400)
+model.fit(rescaledX, Y_train)
+
+# transform the validation dataset
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+print(mean_squared_error(Y_validation, predictions))
+print(predictions)
